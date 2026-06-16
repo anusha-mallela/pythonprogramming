abeecbe3b395661b206783b2036d8794ed512afa
anusha-mallela <anushamallela23@gmail.com>
2026-06-16 10:47:37 +0530

    Create personalinfo.py

---
+++ personalinfo.py
@@ -0,0 +1,8 @@
+name = "Anusha"
+roll_no = "25kn1d0520"
+branch = "Computer Science and Engineering"
+college = "NRIU Engineering College"
+print("Name:", name)
+print("Roll Number:", roll_no)
+print("Branch:", branch)
+print("College Name:", college)
