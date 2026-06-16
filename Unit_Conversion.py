adcfe4d7072d4603fbc1711f6d5adafed13577b3
Anusha_Mallela <anushamallela23@gmail.com>
2026-06-16 15:51:16 +0530

    Create Unit_Conversion.py

---
+++ Unit_Conversion.py
@@ -0,0 +1,8 @@
+kilometers = float(input("Enter distance in kilometers: "))
+meters = kilometers * 1000
+centimeters = meters * 100
+print("Meters =", meters)
+print("Centimeters =", centimeters)
+kilograms = float(input("Enter weight in kilograms: "))
+grams = kilograms * 1000
+print("Grams =", grams)
